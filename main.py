00435aadde51880736eb2e3a0cbb7c78c704ecbe
Jawad Zaman <jawad.zaman@hotmail.co.uk>
2026-08-11 00:50:47 +0100

    basic val rank and some text easter eggs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 import discord
@@ -10,6 +11,10 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+INITIAL_EXTENSIONS = [
+    "cogs.valorant",
+]
 
 
 @bot.event
@@ -25,7 +30,21 @@ async def on_message(message):
     if message.content.strip().lower() == "jbc bot":
         await message.channel.send("hi")
 
+    if message.content.strip().lower() == "ano":
+        await message.channel.send("hello ano")
+
+    if message.content.strip().lower() == "vookum":
+            await message.channel.send("AI LONDON OPEN THAT SHIT UP")
+
     await bot.process_commands(message)
 
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+async def main():
+    async with bot:
+        for extension in INITIAL_EXTENSIONS:
+            await bot.load_extension(extension)
+        await bot.start(os.getenv("DISCORD_TOKEN"))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
